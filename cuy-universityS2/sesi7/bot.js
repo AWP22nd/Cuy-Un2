28eba940a5cf5688aaa8fdb0ea1bd0c7bb4a01d5
AWP22nd <aaawwwppp22@gmail.com>
2026-08-26 20:56:58 +0700

    Add finishing and botEnd functions to bot.js(uncompleted)

--- cuy-universityS2/sesi7/bot.js
+++ cuy-universityS2/sesi7/bot.js
@@ -14,6 +14,24 @@ q.innerHTML = botSay()[0]
 function botStart() {
     init++
     if (init === 1) {
-        console.log("HI BOSS")
+        console.log({name: a.value})
+    } else if (init === 2) {
+        console.log({age: a.value})
+    } else if (init === 3) {
+        console.log({hobby: a.value})
+    } else if (init === 4) {
+        console.log({favFood: a.value})
+    } else if (init ===5) {
+        finishing()
+    } else {
+        botEnd()
     }
+}
+
+function finishing() {
+    console.log("Finishing . . .")
+}
+
+function botEnd() {
+    console.log("Bot end . . .")
 }
