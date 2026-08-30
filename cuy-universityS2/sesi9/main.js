f861edec97fbafe46c743bec9f9688fbd05f3605
AWP22nd <aaawwwppp22@gmail.com>
2026-08-30 17:06:42 +0700

    Refactor fetch to async/await in main.js
    Refactored API call to use async/await for better readability.

--- cuy-universityS2/sesi9/main.js
+++ cuy-universityS2/sesi9/main.js
@@ -5,6 +5,13 @@ console.log("testing . . ")
 // const endPoint = "https://reqres.in/api/users/3"  //select one by one id from each own id
 const endPoint = "https://reqres.in/api/users"   //all id
 
-fetch(endPoint, {
-    .then((result) => result.json())
-    .then((data) => console.log(data))
+// fetch(endPoint, {
+//     .then((result) => result.json())
+//     .then((data) => console.log(data))
+
+async function hitAPI() {
+    const api = await fetch(endPoint)
+    const {data} = await api.json()
+    console.log(data)
+}
+hitAPI()
