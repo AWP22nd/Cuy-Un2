046de2b4926a278fcccaa975941ced93d18dfa07
AWP22nd <aaawwwppp22@gmail.com>
2026-08-30 17:06:54 +0700

    Change fetch method to POST with user data

--- cuy-universityS2/sesi9/main.js
+++ cuy-universityS2/sesi9/main.js
@@ -5,13 +5,22 @@ console.log("testing . . ")
 // const endPoint = "https://reqres.in/api/users/3"  //select one by one id from each own id
 const endPoint = "https://reqres.in/api/users"   //all id
 
-// fetch(endPoint, {
-//     .then((result) => result.json())
-//     .then((data) => console.log(data))
+fetch(endPoint, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        email: "aaawwppp22@gmail.com",
+        fisrtName: "skeb",
+    })
+})
+    .then((result) => result.json())
+    .then((data) => console.log(data))
 
-async function hitAPI() {
-    const api = await fetch(endPoint)
-    const {data} = await api.json()
-    console.log(data)
-}
-hitAPI()
+// async function hitAPI() {
+//     const api = await fetch(endPoint)
+//     const {data} = await api.json()
+//     console.log(data)
+// }
+// hitAPI()
