9ccc392a3d1503900b1b6693d6ec82ceb47d5d60
AWP22nd <aaawwwppp22@gmail.com>
2026-08-30 17:06:12 +0700

    Fix fetch API call and response handling
    Updated the fetch call to properly handle the response.

--- cuy-universityS2/sesi9/main.js
+++ cuy-universityS2/sesi9/main.js
@@ -1,5 +1,10 @@
+//Fetching External API
+
 console.log("testing . . ")
 
-const endPoint = "https://reqres.in/api/users"
+// const endPoint = "https://reqres.in/api/users/3"  //select one by one id from each own id
+const endPoint = "https://reqres.in/api/users"   //all id
 
-(fetch(endPoint)).then((data) => console.log(data))
+fetch(endPoint, {
+    .then((result) => result.json())
+    .then((data) => console.log(data))
